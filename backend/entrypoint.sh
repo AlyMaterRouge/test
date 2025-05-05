00b853c348d0c73f72654d67130ca6c465e2431a
alymatar <125472315+alymatar@users.noreply.github.com>
2025-05-05 19:26:07 +0300

    Update entrypoint.sh

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -12,13 +12,7 @@ x11vnc -forever -shared -nopw -display :99 &
 # Start noVNC without authentication
 # use the PORT env that Render sets
 # Serve /opt/novnc as static under /novnc, proxy WS at /novnc/websockify
-/opt/novnc/utils/novnc_proxy \
-  --web /opt/novnc \
-  --wrap-mode=once \
-  --web=/opt/novnc \
-  --ws-path /novnc/websockify \
-  --vnc localhost:5900 \
-  --listen $PORT
+/opt/novnc/utils/novnc_proxy --vnc localhost:5900 --listen $PORT
 
 
 
